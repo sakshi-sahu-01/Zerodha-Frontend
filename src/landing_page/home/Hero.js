8ce03a37e85bc1df7e9dcb96a0c4156b35579ec7
Sakshi Sahu <sahusaksho889@gmail.com>
2025-08-14 19:05:38 +0530

    files

--- src/landing_page/home/Hero.js
+++ src/landing_page/home/Hero.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React from "react";
 
 function Hero() {
-    return ( 
-      <div className="container p-5 mb-5">
-            <div className="row text-center">
-               <img src='images/homeHero.png' alt="Hero Image" className='mb-5' />
-                   <h1 className='mt-'> Invest in everthing</h1>
-                   <p>Online Plateform to invert in stocks, derivatives, mutual finds & more</p>
-                   <Link to={'/signup'}>
-                   <button  className="p-2 btn btn-primary fs-5 mb-5" style={{width:"20%", margin: "0 auto"}}> Sign Up </button></Link>
-            </div>
+  return (
+    <div className="container p-5 mb-5">
+      <div className="row text-center">
+        <img
+          src="media/images/homeHero.png"
+          alt="Hero Image"
+          className="mb-5"
+        />
+        <h1 className="mt-5">Invest in everything</h1>
+        <p>
+          Online platform to invest in stocks, derivatives, mutual funds, and
+          more
+        </p>
+        <button
+          className="p-2 btn btn-primary fs-5 mb-5"
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          Signup Now
+        </button>
       </div>
-
-     );
+    </div>
+  );
 }
 
 export default Hero;
