1e078b7a5f1238a3ff53c1e05712c03835576356
Sakshi Sahu <sahusaksho889@gmail.com>
2025-08-17 20:51:05 +0530

    files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css';
 import "react-toastify/dist/ReactToastify.css";
 
-import Homepage from './landing_page/Home/HomePage';
+import Homepage from './landing_page/Home/Homepage';
 
 
 
